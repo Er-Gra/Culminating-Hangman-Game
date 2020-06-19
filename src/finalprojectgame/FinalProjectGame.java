de58db084e9f13a8e70f5ace6f7e1bb5718130d6
kikag <kikag@192.168.2.55>
2020-06-19 16:14:18 -0400

    Hangman game culminating 2020

---
+++ src/finalprojectgame/FinalProjectGame.java
@@ -0,0 +1,21 @@
+/*
+ * Date
+ * Name
+ * Program
+ */
+package finalprojectgame;
+
+/**
+ *
+ * @author kikag
+ */
+public class FinalProjectGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
